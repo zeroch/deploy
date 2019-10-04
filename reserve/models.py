a3558d7ec77d1acc2ae2de3d4f7af3243f6bcd7c
Ze Chen <ze.chen@grabtaxi.com>
2019-10-03 20:03:03 -0700

    doctor and patient model

--- reserve/models.py
+++ reserve/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+    name = models.CharField(max_length=255)
+    balance = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Patient(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
---
+++ reserve/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from reserve.models import Doctor, Patient
+
+class DoctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Doctor
+        fields = "__all__"
+
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patient
+        fields = "__all__"
+    
+
