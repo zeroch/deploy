e9aa7347079b523bf6e97f02e3a40ba74a957bcf
Ze Chen <ze.chen@grabtaxi.com>
2019-10-06 13:19:07 -0700

    change serializer for patient name

--- reserve/serializers.py
+++ reserve/serializers.py
@@ -12,9 +12,16 @@ class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
         fields = "__all__"
+    
 
 class AppointmentSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Appointment
         fields = ('doctor','start_time','event_date', 'kind', 'patient')
+
+    def to_representation(self, instance):
+        rep = super(AppointmentSerializer, self).to_representation(instance)
+        rep['kind'] = instance.get_kind_display()
+        rep['patient'] = instance.patient.last_name + " " + instance.patient.first_name
+        return rep
